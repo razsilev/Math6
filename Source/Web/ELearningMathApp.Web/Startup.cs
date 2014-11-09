711b1787f00f76b3fba26fab6eeba9a372e10f5b
razsilev <grasilev@gmail.com>
2014-11-09 21:08:05 +0200

    Create empty data and web projects

---
+++ Source/Web/ELearningMathApp.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ELearningMathApp.Web.Startup))]
+namespace ELearningMathApp.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
