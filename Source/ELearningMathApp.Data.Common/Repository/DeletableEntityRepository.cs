370102b3a216fbecabd3eaba2946b69e49621f9e
razsilev <grasilev@gmail.com>
2014-11-10 00:40:45 +0200

    Implemented repository interfaces

---
+++ Source/ELearningMathApp.Data.Common/Repository/DeletableEntityRepository.cs
@@ -0,0 +1,28 @@
+﻿namespace ELearningMathApp.Data.Common.Repository
+{
+    using ELearningMathApp.Data.Common.Models;
+    using ELearningMathApp.Data.Common.Repository;
+    using System.Data.Entity;
+    using System.Linq;
+
+    //using ELearningMathApp.Data.Common.;
+
+    public class DeletableEntityRepository<T> : GenericRepository<T>, IDeletableEntityRepository<T>
+        where T : class, IDeletableEntity
+    {
+        public DeletableEntityRepository(DbContext context)
+            : base(context)
+        {
+        }
+
+        public override IQueryable<T> All()
+        {
+            return base.All().Where(x => !x.IsDeleted);
+        }
+
+        public IQueryable<T> AllWithDeleted()
+        {
+            return base.All();
+        }
+    }
+}
