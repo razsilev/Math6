6fc1e8231b0361331405ce402695e72fbe7d8100
razsilev <grasilev@gmail.com>
2014-11-15 11:20:53 +0200

    Add drop down menu to layout

---
+++ Source/Web/ELearningMathApp.Web/Controllers/IdeasController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ELearningMathApp.Web.Controllers
+{
+    public class IdeasController : Controller
+    {
+        // GET: Ideas
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
